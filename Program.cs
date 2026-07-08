a829324be0aa0483ecd2935c7ec8e35381e9767c
Mohammed Sajjad <mohammedsajjad1017@gmail.com>
2026-07-08 11:13:27 -0500

    Database Migration

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using MomoMats.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 
@@ -7,6 +8,7 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 builder.Services.AddSwaggerGen();
+
 builder.Services.AddDbContext<MomoMatsDbContext>(options =>
 {
     string connectionString =
